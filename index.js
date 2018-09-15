0e521712d17e8b2f35dedb36c971f4eac3aaeefb
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2018-09-15 18:32:45 +0100

    Line-based filters now available.

--- index.js
+++ index.js
@@ -205,6 +205,84 @@ const greengreyscale_imgdata = () => {
 }
 
 
+const add_horizontal_line_imgdata = () => {
+    let inc = 0;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        inc += 1;
+        if (inc > 255) {
+            inc = 0;
+        }
+        let avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+        imgData.data[i] = avg + inc;
+        imgData.data[i + 1] = avg + 70
+        imgData.data[i + 2] = avg + 20
+    }
+    return imgData;
+}
+
+const add_diagonal_lines_imgdata = () => {
+    let inc = 0;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        inc += 20;
+        if (inc > 255) {
+            inc = 0;
+        }
+        let avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+        imgData.data[i] = avg + inc;
+        imgData.data[i + 1] = avg + 70
+        imgData.data[i + 2] = avg + 20
+    }
+    return imgData;
+}
+
+const add_green_diagonal_lines_imgdata = () => {
+    let inc = 0;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        inc += 20;
+        if (inc > 255) {
+            inc = 0;
+        }
+        var avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+        imgData.data[i] = avg + 5;
+        imgData.data[i + 1] = avg + inc;
+        imgData.data[i + 2] = avg + 20
+    }
+    return imgData;
+}
+
+const pane_imgdata = () => {
+    let inc = 0;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        inc += 200;
+        if (inc > 255) {
+            inc = 0;
+        }
+        let avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+        imgData.data[i] = avg + 5;
+        imgData.data[i + 1] = avg + inc;
+        imgData.data[i + 2] = avg + 20
+    }
+    return imgData;
+}
+
+const casino_imgdata = () => {
+
+    let inc = 0;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        inc = getRandomNumber(0, 255);
+        if (inc > 255) {
+            inc = 0;
+        }
+
+        let avg = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3
+        imgData.data[i] = avg + inc;
+        imgData.data[i + 1] = avg + 2;
+        imgData.data[i + 2] = avg + 5;
+    }
+    return imgData;
+}
+
 
 const imgToCanvas = (img) => {
     var canvas = document.createElement("canvas");
