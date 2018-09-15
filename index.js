7412e16e234db5a57f7b59b2aad3a69d6d8085d0
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2018-09-15 18:36:16 +0100

    Enhanced noise filters by adding tints to noise filters.

--- index.js
+++ index.js
@@ -801,6 +801,238 @@ const horizon_imgdata = () => {
     return imgData;
 }
 
+const evening_imgdata = () => {
+    let SAT_ADJ = 60;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] -= SAT_ADJ
+        imgData.data[i + 1] -= SAT_ADJ
+        imgData.data[i + 2] -= SAT_ADJ
+    }
+    return imgData;
+}
+
+
+// An experimental function that is used for the testing of image filtering.
+const a = function () {
+    let randomNumber = 0;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        randomNumber = getRandomNumber(0, 200);
+        let addition = 0;
+        if (randomNumber > 0 && randomNumber < 50) {
+            addition1 = 0;
+            addition2 = 30;
+        }
+        else if (randomNumber > 49 && randomNumber < 100) {
+            addition1 = 100;
+            addition2 = 90;
+        }
+
+        else {
+            addition1 = 70;
+            addition2 = 10;
+        }
+
+        if (imgData.data[i] - addition > 255) {
+            imgData.data[i] -= addition
+        }
+        else {
+            imgData.data[i] += addition
+        }
+
+        if (imgData.data[i + 1] + addition > 255) {
+            imgData.data[i + 1] -= addition2;
+        } else {
+            imgData.data[i + 2] += addition2;
+        }
+    }
+    return imgData;
+}
+
+const pixel_blue_imgdata = () => {
+    let randomNumber = 0;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        randomNumber = getRandomNumber(0, 200);
+        let addition = 0;
+        if (randomNumber > 0 && randomNumber < 50) {
+            addition1 = 0;
+            addition2 = 30;
+        }
+        else if (randomNumber > 49 && randomNumber < 100) {
+            addition1 = 100;
+            addition2 = 90;
+        }
+
+        else {
+            addition1 = 70;
+            addition2 = 10;
+        }
+
+        if (imgData.data[i] - addition > 255) {
+            imgData.data[i] -= addition
+        }
+        else {
+            imgData.data[i] += addition
+        }
+
+        if (imgData.data[i + 1] + addition > 255) {
+            imgData.data[i + 1] -= addition2;
+        } else {
+            imgData.data[i + 2] += addition2;
+        }
+    }
+    return imgData;
+}
+
+
+const lemon_imgdata = () => {
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i + 1] = imgData.data[i] + 50;
+    }
+    return imgData;
+}
+
+const coral_imgdata = () => {
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i + 2] = imgData.data[i + 1] + 50;
+    }
+    return imgData;
+}
+
+const frontward_imgdata = () => {
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = imgData.data[i + 2];
+        imgData.data[i + 2] = imgData.data[i + 1] + 50;
+    }
+    return imgData;
+}
+
+const green_med_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 9;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.5;
+
+        let randomColor2 = 0.6 + Math.random() * 0.5;
+
+        let randomColor3 = 0.6 + Math.random() * 0.5;
+
+        imgData.data[i] = imgData.data[i + 1] * 0.5 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 2 ] * 0.99 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
+
+const dark_purple_min_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 9;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.5;
+
+        let randomColor2 = 0.6 + Math.random() * 0.5;
+
+        let randomColor3 = 0.6 + Math.random() * 0.5;
+
+        imgData.data[i] = imgData.data[i] * 0.5 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 1] * 0.3 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i + 2] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
+
+const teal_min_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 1;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.1;
+
+        let randomColor2 = 0.6 + Math.random() * 0.5;
+
+        let randomColor3 = 0.6 + Math.random() * 0.5;
+
+        imgData.data[i] = imgData.data[i] * 0.99 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 1] * 0.99 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i + 2] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
+
+const purple_min_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 1;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.5;
+
+        let randomColor2 = 0.6 + Math.random() * 0.2;
+
+        let randomColor3 = 0.6 + Math.random() * 0.8;
+
+        imgData.data[i] = imgData.data[i] * 0.99 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 1] * 0.99 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i + 2] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
+
+const blue_min_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 1;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.1;
+
+        let randomColor2 = 0.6 + Math.random() * 0.2;
+
+        let randomColor3 = 0.6 + Math.random() * 0.7;
+
+        imgData.data[i] = imgData.data[i] * 0.99 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 1] * 0.99 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i + 2] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
+
+const green_min_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 1;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.1;
+
+        let randomColor2 = 0.6 + Math.random() * 0.5;
+
+        let randomColor3 = 0.6 + Math.random() * 0.4;
+
+        imgData.data[i] = imgData.data[i] * 0.99 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 1] * 0.99 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i + 2] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
+
+const pink_min_noise_imgdata = () => {
+    let rand = (0.5 - Math.random()) * 1;
+
+    for (i = 0; i < imgData.data.length; i += 4) {
+        let randomColor1 = 0.6 + Math.random() * 0.6;
+
+        let randomColor2 = 0.6 + Math.random() * 0.1;
+
+        let randomColor3 = 0.6 + Math.random() * 0.4;
+
+        imgData.data[i] = imgData.data[i] * 0.99 * randomColor1; 
+        imgData.data[i + 1] = imgData.data[i + 1] * 0.99 * randomColor2;
+
+        imgData.data[i + 2] = imgData.data[i + 2] * 0.99 * randomColor3; 
+    }
+    return imgData;
+}
 
 const imgToCanvas = (img) => {
     var canvas = document.createElement("canvas");
