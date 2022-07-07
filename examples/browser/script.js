de51bcb4d78ade04559be5ac66b8d5006ff9be7e
Silvia O'Dwyer <silviaodwyer119@yahoo.ie>
2022-07-07 14:53:14 +0100

    Updated brightness adj filters and updated demo example; issue #4

--- examples/browser/script.js
+++ examples/browser/script.js
@@ -3,18 +3,21 @@
     document.addEventListener('DOMContentLoaded', init, false);
 
     function init() {
-        
+        console.log("init")
         var img = document.getElementById("img");
+        img.crossOrigin = "Anonymous";
         img.onload = function () {
             pixelsJS.filterImg(img, "neue");
         }
 
         var img2 = document.getElementById("img2");
+        img2.crossOrigin = "Anonymous";
         img2.onload = function() {
             pixelsJS.filterImg(img2, "cosmic")
         }
 
         var img3 = document.getElementById("img3");
+        img3.crossOrigin = "Anonymous";
         img3.onload = function() {
             pixelsJS.filterImg(img3, "ocean")
         }
